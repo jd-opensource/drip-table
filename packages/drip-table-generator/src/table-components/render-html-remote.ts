fd9651a52b54d5839fbb7c32ef8c58bb701526a7
helloqian12138 <johnhello12138@163.com>
2022-04-26 10:30:16 +0800

    feat: generator add remote url component

---
+++ packages/drip-table-generator/src/table-components/render-html-remote.ts
@@ -0,0 +1,27 @@
+import { DripTableComponentAttrConfig } from '../typing';
+import { basicColumnAttrComponents } from './configs';
+
+export default {
+  $id: '$table_renderhtmlremote',
+  'ui:type': 'render-html-remote',
+  type: 'string',
+  group: '自定义组件',
+  fieldKey: 'render-html-remote',
+  title: 'CDN链接组件',
+  paramName: '',
+  default: '',
+  attrSchema: [
+    ...basicColumnAttrComponents,
+    {
+      name: 'options.url',
+      group: '组件属性',
+      'ui:title': '代码地址',
+      'ui:type': 'input',
+      'ui:props': {
+        placeholder: '请输入代码地址，如：https://unpkg.com/react.development.js',
+      },
+      type: 'string',
+    },
+  ],
+  icon: '<svg t="1627277503592" class="icon" viewBox="0 0 1097 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5523" width="48" height="48"><path d="M352.585143 799.414857l-28.562286 28.562286a18.285714 18.285714 0 0 1-26.294857 0L31.451429 561.700571a18.285714 18.285714 0 0 1 0-26.294857l266.276571-266.276571a18.285714 18.285714 0 0 1 26.294857 0l28.562286 28.562286a18.285714 18.285714 0 0 1 0 26.294857L128 548.571429l224.585143 224.585142a18.285714 18.285714 0 0 1 0 26.294858z m337.700571-609.718857l-213.138285 737.718857a18.139429 18.139429 0 0 1-22.272 12.580572l-35.437715-9.728a18.505143 18.505143 0 0 1-12.580571-22.857143L619.995429 169.691429a18.139429 18.139429 0 0 1 22.272-12.580572l35.437714 9.728a18.505143 18.505143 0 0 1 12.580571 22.857143z m375.442286 372.004571L799.451429 827.977143a18.285714 18.285714 0 0 1-26.294858 0l-28.562285-28.562286a18.285714 18.285714 0 0 1 0-26.294857l224.585143-224.585143-224.585143-224.585143a18.285714 18.285714 0 0 1 0-26.294857l28.562285-28.562286a18.285714 18.285714 0 0 1 26.294858 0l266.276571 266.276572a18.285714 18.285714 0 0 1 0 26.294857z" fill="" p-id="5524"></path></svg>',
+} as DripTableComponentAttrConfig;
