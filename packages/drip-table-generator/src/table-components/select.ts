2f6f6d5ff202e769b41d6037a90372e81658e651
helloqian12138 <johnhello12138@163.com>
2022-08-15 20:39:27 +0800

    feat: update tips for boolean

--- packages/drip-table-generator/src/table-components/select.ts
+++ packages/drip-table-generator/src/table-components/select.ts
@@ -94,7 +94,7 @@ export default {
       'ui:title': '禁用组件条件',
       'ui:type': 'text',
       'ui:description': {
-        title: '参数为rec, 可以填写代码片段或者，直接填 true/false',
+        title: '可以直接填写 true/false 或者代码片段，参数为rec',
         trigger: 'hover',
         type: 'icon',
       },
@@ -127,7 +127,7 @@ export default {
             'ui:title': '禁用条件',
             'ui:type': 'text',
             'ui:description': {
-              title: '参数为rec, 可以填写代码片段或者，直接填 true/false',
+              title: '可以直接填写 true/false 或者代码片段，参数为rec',
               trigger: 'hover',
               type: 'icon',
             },
