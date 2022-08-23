42c3a9f403fec1492f78e1a59eba106abef94483
renwenci <renwenci@github.com>
2022-08-23 18:22:19 +0800

    feat: change file header

--- packages/drip-table/src/layouts/card/index.tsx
+++ packages/drip-table/src/layouts/card/index.tsx
@@ -1,8 +1,8 @@
-/*
+/**
  * This file is part of the drip-table project.
  * @link     : https://drip-table.jd.com/
- * @author   : Emil Zhai (root@derzh.com)
- * @modifier : Emil Zhai (root@derzh.com)
+ * @author   : renwenci (ararakikon@163.com)
+ * @modifier : renwenci (ararakikon@163.com)
  * @copyright: Copyright (c) 2021 JD Network Technology Co., Ltd.
  */
 
