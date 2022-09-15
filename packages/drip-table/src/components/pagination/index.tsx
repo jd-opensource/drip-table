e62b6eeeb3ee9ad337571d27115a9febd2f2b025
Emil Zhai <root@derzh.com>
2022-09-15 16:50:20 +0800

    fix: pagination jump icon

--- packages/drip-table/src/components/pagination/index.tsx
+++ packages/drip-table/src/components/pagination/index.tsx
@@ -43,6 +43,30 @@ const Pagination = React.memo((props: RcPaginationProps & { align?: 'left' | 'ce
         </span>
       </button>
     )}
+    jumpPrevIcon={(
+      <a className={styles['jfe-drip-table-pagination-item-link']}>
+        <div className={styles['jfe-drip-table-pagination-item-container']}>
+          <span role="img" aria-label="double-left" className={styles['jfe-drip-table-pagination-item-link-icon']}>
+            <svg viewBox="64 64 896 896" focusable="false" data-icon="double-left" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+              <path d="M272.9 512l265.4-339.1c4.1-5.2.4-12.9-6.3-12.9h-77.3c-4.9 0-9.6 2.3-12.6 6.1L186.8 492.3a31.99 31.99 0 000 39.5l255.3 326.1c3 3.9 7.7 6.1 12.6 6.1H532c6.7 0 10.4-7.7 6.3-12.9L272.9 512zm304 0l265.4-339.1c4.1-5.2.4-12.9-6.3-12.9h-77.3c-4.9 0-9.6 2.3-12.6 6.1L490.8 492.3a31.99 31.99 0 000 39.5l255.3 326.1c3 3.9 7.7 6.1 12.6 6.1H836c6.7 0 10.4-7.7 6.3-12.9L576.9 512z" />
+            </svg>
+          </span>
+          <span className={styles['jfe-drip-table-pagination-item-ellipsis']}>•••</span>
+        </div>
+      </a>
+    )}
+    jumpNextIcon={(
+      <a className={styles['jfe-drip-table-pagination-item-link']} one-link-mark="yes">
+        <div className={styles['jfe-drip-table-pagination-item-container']}>
+          <span role="img" aria-label="double-right" className={styles['jfe-drip-table-pagination-item-link-icon']}>
+            <svg viewBox="64 64 896 896" focusable="false" data-icon="double-right" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+              <path d="M533.2 492.3L277.9 166.1c-3-3.9-7.7-6.1-12.6-6.1H188c-6.7 0-10.4 7.7-6.3 12.9L447.1 512 181.7 851.1A7.98 7.98 0 00188 864h77.3c4.9 0 9.6-2.3 12.6-6.1l255.3-326.1c9.1-11.7 9.1-27.9 0-39.5zm304 0L581.9 166.1c-3-3.9-7.7-6.1-12.6-6.1H492c-6.7 0-10.4 7.7-6.3 12.9L751.1 512 485.7 851.1A7.98 7.98 0 00492 864h77.3c4.9 0 9.6-2.3 12.6-6.1l255.3-326.1c9.1-11.7 9.1-27.9 0-39.5z" />
+            </svg>
+          </span>
+          <span className={styles['jfe-drip-table-pagination-item-ellipsis']}>•••</span>
+        </div>
+      </a>
+    )}
     selectPrefixCls="jfe-drip-table-select"
     selectComponentClass={props.size === 'small' ? MiniSelect : Select}
   />
