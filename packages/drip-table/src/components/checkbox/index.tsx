32a90aad913ab3e592a1a352b1a7423b9cef049b
Emil Zhai <root@derzh.com>
2022-09-29 15:02:32 +0800

    fix: checkbox component wrapper border

--- packages/drip-table/src/components/checkbox/index.tsx
+++ packages/drip-table/src/components/checkbox/index.tsx
@@ -6,18 +6,21 @@
  * @copyright: Copyright (c) 2021 JD Network Technology Co., Ltd.
  */
 
-import './index.module.less';
-
+import classNames from 'classnames';
 import RcCheckbox, { Props as RcCheckboxProps } from 'rc-checkbox';
 import React from 'react';
+
+import styles from './index.module.less';
 
 export interface SelectProps extends RcCheckboxProps {}
 
 const Checkbox = React.memo(({ ...props }: SelectProps) => (
-  <RcCheckbox
-    {...props}
-    prefixCls="jfe-drip-table-checkbox"
-  />
+  <label className={classNames(styles['jfe-drip-table-checkbox-wrapper'], styles['jfe-drip-table-checkbox-wrapper-checked'])}>
+    <RcCheckbox
+      {...props}
+      prefixCls="jfe-drip-table-checkbox"
+    />
+  </label>
 ));
 
 export default Checkbox;
