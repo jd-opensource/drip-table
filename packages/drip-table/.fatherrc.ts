ca12a763e555ab4083b8399d48832a84398ef451
johnhello12138 <johnhello12138@163.com>
2023-04-11 18:33:50 +0800

    ci: table output unminified and minified same time

--- packages/drip-table/.fatherrc.ts
+++ packages/drip-table/.fatherrc.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'father';
+import path from 'path';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
 
 export default defineConfig({
   umd: {
@@ -6,6 +8,18 @@ export default defineConfig({
       'src/index': {
         output: 'dist',
       },
+    },
+    chainWebpack(config, { webpack }) {
+      config.entry('index').add(path.join(__dirname, './src/index.ts'));
+      config.entry('index.min').add(path.join(__dirname, './src/index.ts'));
+      config.optimization.minimizer('css').use(TerserWebpackPlugin, [{
+        test: /min/u,
+        terserOptions: {
+          module: true,
+        },
+        minify: v => new Promise(resolve => resolve({ code: v.value })),
+      }]);
+      return config;
     },
   },
   cjs: {
