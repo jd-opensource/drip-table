772659b34f146de086157a679254ee4c32126693
Emil Zhai <root@derzh.com>
2024-05-30 02:06:38 +0000

    fix: filters icon click prevent event

--- packages/drip-table/src/layouts/table/components/header-cell/index.tsx
+++ packages/drip-table/src/layouts/table/components/header-cell/index.tsx
@@ -304,7 +304,7 @@ const HeaderCell = <
                     }
                   }}
                 >
-                  <div className={`${prefixCls}-toolbox-icon`}>
+                  <div className={`${prefixCls}-toolbox-icon`} onClick={preventEvent}>
                     <span role="img" aria-label="filter" className={`${prefixCls}-toolbox-icon-filter`}>
                       <svg viewBox="64 64 896 896" focusable="false" data-icon="filter" width="1em" height="1em" fill="currentColor" aria-hidden="true">
                         <path d="M349 838c0 17.7 14.2 32 31.8 32h262.4c17.6 0 31.8-14.3 31.8-32V642H349v196zm531.1-684H143.9c-24.5 0-39.8 26.7-27.5 48l221.3 376h348.8l221.3-376c12.1-21.3-3.2-48-27.7-48z" />
